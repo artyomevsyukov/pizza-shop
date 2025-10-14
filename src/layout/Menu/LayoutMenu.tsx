0d55f5e8b9ebaf9f658577ed34d5c26fb7e7653b
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2025-10-14 12:08:34 +0300

    Layout - Menu

--- src/layout/Menu/LayoutMenu.tsx
+++ src/layout/Menu/LayoutMenu.tsx
@@ -1,11 +1,50 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import CN from "classnames";
+
+import styles from "./Layout.module.css";
+
+import cartIcon from "/cart-icon.svg";
+import avatar from "/avatar.png";
+import menuIcon from "/menu-icon.svg";
+import exitIcon from "/exit-icon.svg";
+import Button from "../../components/Button/Button";
 
 export function LayoutMenu() {
   return (
-    <div>
-      <div>
-        <Link to="/">Menu</Link>
-        <Link to="/cart">Cart</Link>
+    <div className={styles["layout"]}>
+      <div className={styles["sidebar"]}>
+        <div className={styles["user"]}>
+          <img src={avatar} className={styles["avatar"]} alt="Avatar" />
+          <div className={styles["name"]}>Антон Ларичев</div>
+          <div className={styles["email"]}>alaricode@ya.ru</div>
+        </div>
+        <div className={styles["menu"]}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              CN(styles["link"], {
+                [styles.active]: isActive
+              })
+            }
+          >
+            <img className={styles["menu-icon"]} src={menuIcon} alt="Меню" />
+            Меню
+          </NavLink>
+          <NavLink
+            to="/cart"
+            className={({ isActive }) =>
+              CN(styles["link"], {
+                [styles.active]: isActive
+              })
+            }
+          >
+            <img className={styles["menu-icon"]} src={cartIcon} alt="Корзина" />{" "}
+            Корзина
+          </NavLink>
+        </div>
+        <Button className={styles["exit"]}>
+          <img src={exitIcon} alt="Выход" /> Выход
+        </Button>
       </div>
       <div>
         <Outlet />
