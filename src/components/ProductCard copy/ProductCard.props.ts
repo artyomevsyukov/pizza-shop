5ba027af6ce2bf8fc5e907b7e837fc08bec5e7b7
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2025-10-15 10:29:03 +0300

    added component ProductCard

---
+++ src/components/ProductCard copy/ProductCard.props.ts
@@ -0,0 +1,8 @@
+export interface ProductCardProps {
+  productId: number;
+  rating?: number;
+  price: number;
+  title: string;
+  description: string;
+  image: string;
+}
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,12 +1,23 @@
 import stiles from "./Menu.module.css";
 import Heading from "../../components/Heading/Heading";
 import Search from "../../components/Search/Search";
+import ProductCard from "../../components/ProductCard/ProductCard";
 
 export function Menu() {
   return (
-    <div className={stiles["head"]}>
-      <Heading>Menu</Heading>
-      <Search name="search" placeholder="Введите блюдо или состав" />
-    </div>
+    <>
+      <div className={stiles["head"]}>
+        <Heading>Menu</Heading>
+        <Search name="search" placeholder="Введите блюдо или состав" />
+      </div>
+      <ProductCard
+        id={1}
+        rating={4.5}
+        price={300}
+        name="Наслаждение"
+        description=" Салями, руккола, помидоры, оливки"
+        image="/product-demo.png"
+      />
+    </>
   );
 }
